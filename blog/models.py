dc0b978a8399049bcf5b9fa9afc558991d1ae837
Happiness Nneke <happymba11@yahoo.com>
2017-11-18 13:22:56 +0000

    files added

--- blog/models.py
+++ blog/models.py
@@ -20,11 +20,13 @@ class Post(models.Model):
     tag = models.CharField(max_length=30, blank=True, null=True)
     image = models.ImageField(upload_to="images", blank=True, null=True)
     
+    def __str__(self):
+        return self.title
+    
 def publish(self):
         self.published_date = timezone.now()
         self.save()
 
-def __str__(self):
-        return self.title
+
 
 
